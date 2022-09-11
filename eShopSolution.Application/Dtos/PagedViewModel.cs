e49a608e91719243b8359ff921951c71f5eb178e
Khải Phạm <khaisd123@gmail.com>
2022-09-11 14:12:29 +0700

    Add web api

---
+++ eShopSolution.Application/Dtos/PagedViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Application.Dtos
+{
+    public class PagedViewModel<T>
+    {
+
+        public List<T> Item { set; get; }
+        public int TotalRecord { set; get; }
+    }
+}
